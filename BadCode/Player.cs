a4697c8c3c1b6eded87ce081ba46830281a59f4e
Rasmus Bach Jørgensen <rasm74e5@edu.eal.dk>
2019-02-04 08:40:54 +0100

    Add project files.

---
+++ BadCode/Player.cs
@@ -0,0 +1,18 @@
+﻿namespace BadCode
+{
+    public class Player
+    {
+        public string n { get; set; }
+        public string a { get; set; }
+        public string em { get; set; }
+        public string t { get; set; }
+        
+        public Player(string playerName, string playerAddress = null, string playerEmail = null, string playerTelephone = null)
+        {
+            n = playerName;
+            a = playerAddress;
+            em = playerEmail;
+            t = playerTelephone;
+        }
+    }
+}
---
+++ BadCode/Team.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace BadCode
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        private List<Player> players = new List<Player>();
+
+        public Team(string teamName)
+        {
+            Name = teamName;
+        }
+
+        public void AddPlayer(Player p)
+        {
+            players.Add(p);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
